9b7f44a90d378ea13dfce5cefcc97fa95c614134
Henrik Larsson <henrik@hexigames.com>
2022-12-01 19:12:04 +0100

    day 1 part 2 performance improvement

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,3 @@
-use std::cmp::Reverse;
-
 pub fn part_one(input: &str) -> Option<u32> {
     input
         .split("\n\n")
@@ -13,17 +11,29 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut elves = input
+    let elves = input
         .split("\n\n")
-        .map(|elf| {
-            elf.split('\n')
+        .fold((0u32, 0u32, 0u32), |highest_calories, elf| {
+            let calories = elf
+                .split('\n')
                 .map(|item| item.parse::<u32>().unwrap_or_default())
                 .reduce(|acc: u32, cal: u32| cal + acc)
-                .unwrap()
-        })
-        .collect::<Vec<u32>>();
-    elves.sort_by_key(|n| Reverse(*n));
-    Some(elves[0] + elves[1] + elves[2])
+                .unwrap();
+            update_highest_calories(highest_calories, calories)
+        });
+    Some(elves.0 + elves.1 + elves.2)
+}
+
+pub fn update_highest_calories(
+    highest_calories: (u32, u32, u32),
+    new_calories: u32,
+) -> (u32, u32, u32) {
+    match highest_calories {
+        (a, b, _) if new_calories > a => (new_calories, a, b),
+        (a, b, _) if new_calories > b => (a, new_calories, b),
+        (a, b, c) if new_calories > c => (a, b, new_calories),
+        _ => highest_calories,
+    }
 }
 
 fn main() {
